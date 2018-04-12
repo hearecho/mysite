d00b8f78be56a23bcce00753a8a15f416979ec8e
hearecho <1540302560@qq.com>
2018-04-12 22:38:53 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,26 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 #配置 ckeditor
 CKEDITOR_UPLOAD_PATH = 'upload/'
-
+CKEDITOR_CONFIGS = {
+    'default':{
+        
+    },
+    'comment_ckeditor': {
+        'toolbar': 'custom',
+        'toolbar_custom': [
+            ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript'],
+            ["TextColor", "BGColor", 'RemoveFormat'],
+            ['NumberedList', 'BulletedList'],
+            ['Link', 'Unlink'],
+            ["Smiley", "SpecialChar", 'Blockquote'],
+        ],
+        'width': 'auto',
+        'height': '180',
+        'tabSpaces': 4,
+        'removePlugins': 'elementspath',
+        'resize_enabled': False,
+    }
+}
 #缓存设置
 CACHES = {
     'default': {
